d94340de7a89abb267d473d1a213d36dd84ea657
Rogoza Lyuba <lyubashka22rogoza@gmail.com>
2021-09-03 22:41:00 +0300

    start task 4

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 //task1
-function wordSpaces (text) {
-    let numberOfSpacesTags = [ " " , " <br>"]
-    let number = ["пробелов" , "переводов"]
-    let totalSoFar = [];
-   
-   
-    for (var i = 0; i < wordSpaces.length; i++)
-    if (text(i) === " ") { // if a space is found in str
-      totalSoFar = +1; // add 1 to total so far
-    }
-    totalsoFar += 1; // add 1 to totalsoFar to account for extra space since 1 space = 2 words
-  }
+// function count () {
 
-alert("Вы ввели в строку:  "+numberOfSpacesTags.split('') + "в ней "+ "и переводов строки" )
-// wordSpaces.split(' ').length-1
+//   let str = 'hello world ,';
+//   let space = str.split(" ").length-1 + ' пробелов и ';
+//   let br = str.split('br/').length-1 + ' переводов строки ';
+// // alert("Вы ввели стоку: " +"\n"+ str + " в ней " + space + br );
+// }
+// count ()
+//task2
+
+// function makeid() {
+    
+//     let months = ["January", "February", "March", "April", "May", "June", "July"];
+
+//     let random = Math.floor(Math.random() * months.length);
+    
+//     console.log(random, months[random]);
+// }
+// makeid()
+
+//task3
+
+// function smartSplitAll (strArr) {
+//     var newArr = [];
+//     for (var i = 0; i < strArr.length; i++) {
+//       var str = strArr[i].trim();
+//       if (str.indexOf(' ') > -1)
+//           newArr = newArr.concat(str.split(/\s+/));
+//       else 
+//           newArr.push(str);
+//     }
+
+//     return newArr;
+//   }
+  
+//   console.log(smartSplitAll(["firstName lastName", "anotherString"]));
+//   // result: ["firstName", "lastName", "anotherString"]
+
+let arr = ["one" , "two"];
+
+function returnSplitString(string){
+   return string.split(" ");
+}
+
+let splitStringArray = returnSplitString(arr[0]);
+console.log (arr);
